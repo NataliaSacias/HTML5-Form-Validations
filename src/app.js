dc5915263472a04db5faf1dc324aefb8f040c469
NataliaSacias <nsacias@gmail.com>
2021-04-21 18:30:41 +0000

    usando validator js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
+import validator from "validator";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// window.onload = function() {
+//   //write your code here
+//   console.log("Hello Rigo from the console!");
+// };
+let form;
